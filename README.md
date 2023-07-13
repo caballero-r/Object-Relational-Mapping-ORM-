4bff697f27976302463ffdcd4b0667a4c8c490b2
Russell <rnc720@gmail.com>
2023-07-13 13:03:03 -0500

    Logic for Category-Routes: - Added logic for call all categories - Added logic for call category by id - Added logic to creat new category - Added logic to update category by id - Added logic to delete a category by id

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Object Relational Mapping (ORM)
 
 <!-- 
+Other contributors:
+1. Xandromus - Starter Code
+2. dependabot[bot] - Starter Code
+3. Georgeyoo - Starter Code
+4. alimaqsood19 - Instructor / Sequelize Example
+
+
 Place holder for references used: 
 1. Sequelize v6 - https://sequelize.org/docs/v6/ 
+2. Sequelize Example - https://github.com/alimaqsood19/sequelize_Example
 -->
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,89 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product}],
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    });
+
+    // This checks to see if the category id exists. If it does not the user is provided with a message.
+    if (!category) {
+      res.status(404).json({ message: 'No category was found under this id. Please use existing category id.' })
+      return;
+    }
+
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryInfo = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    // This checks to see if the category id exists. If it does not the user is provided with a message.
+    if (!categoryInfo) {
+      res.status(404).json({ message: 'No category was found under this id. Please use existing category id.' })
+      return;
+    }
+    res.status(200).json({ message: 'Category Name was successfully updated. Thank you!'});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const removed = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    // This checks to see if the category id exists. If it does not the user is provided with a message.
+    if (!removed) {
+      res.status(404).json({ message: 'No category was found under this id. Please use existing category id.' })
+      return;
+    }
+    res.status(200).json({ message: 'This category has been successfully removed from database.'});
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
